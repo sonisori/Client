c29815b4c475770a39424f2c7bf66a0b46706d54
Donghyun Lee <leedonghyun022@gmail.com>
2024-10-04 14:27:00 +0900

    feat: 스플래시 추가

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,16 @@
+import { Router } from "@solidjs/router";
+
+import { Splash } from "./screen/Splash";
+
 export const App = () => {
-  const a = 1;
-  if (a) {
-    return (
-      <div class="container mx-auto">
-        <h1>소니소리 웹 </h1>
-      </div>
-    );
-  }
+  return (
+    <Router>
+      {[
+        {
+          path: "/",
+          component: () => <Splash>내용 추가</Splash>,
+        },
+      ]}
+    </Router>
+  );
 };
--- web/src/screen/Splash.tsx
+++ web/src/screen/Splash.tsx
@@ -1,3 +1,23 @@
-export const Splash = () => {
-  return <div class="fixed inset-0 bg-gray-50">sdff</div>;
+import { createSignal, JSXElement, Show } from "solid-js";
+
+export const Splash = (props: { children: JSXElement }) => {
+  const [loading, setLoading] = createSignal(true);
+
+  setTimeout(() => {
+    // setLoading(false);
+  }, 2000);
+
+  return (
+    <>
+      <Show when={loading()}>
+        <div class="fixed inset-0 flex items-center justify-center bg-gray-50">
+          <div class="grid grid-cols-2 items-center justify-items-end gap-10">
+            <div class="size-32 bg-gray-200" />
+            <h1 class="text-6xl font-semibold">SoniSori</h1>
+          </div>
+        </div>
+      </Show>
+      <Show when={!loading()}>{props.children}</Show>
+    </>
+  );
 };
