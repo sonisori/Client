72f7412ec850e9d32399540b575c50bbc95bfef4
Donghyun Lee <leedonghyun022@gmail.com>
2024-10-06 13:14:10 +0900

    feat: shadcn/ui 추가

--- web/src/ui/screen/SignIn.tsx
+++ web/src/ui/screen/SignIn.tsx
@@ -7,10 +7,8 @@ export const SignIn = () => {
       <div class="px-5 py-20">
         <p class="text-2xl font-medium">소셜 로그인으로 시작하세요</p>
         <div class="mt-10 flex gap-4">
-          <Button style={{ "background-color": "#03C75A" }}>NAVER</Button>
-          <Button style={{ "background-color": "#FEE500", color: "#000" }}>
-            Kakao
-          </Button>
+          <Button>NAVER</Button>
+          <Button>Kakao</Button>
         </div>
       </div>
     </LogoLayout>
