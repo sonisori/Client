9015ebf651c51d29afa88bf386792797c865ac51
Donghyun Lee <leedonghyun022@gmail.com>
2024-12-05 17:54:53 +0900

    feat: 로고 추가

--- web/src/ui/screen/Splash.tsx
+++ web/src/ui/screen/Splash.tsx
@@ -13,7 +13,7 @@ export const Splash = (props: { children: JSXElement }) => {
     <Show fallback={props.children} when={loading()}>
       <div class="fixed inset-0 flex items-center justify-center bg-white">
         <div class="grid grid-cols-2 items-center justify-items-end gap-10">
-          <div class="size-32 bg-gray-200" />
+          <img alt="" class="size-32" src="/asset/logo.jpg" />
           <h1 class="text-6xl font-semibold">{SERVICE_NAME}</h1>
         </div>
       </div>
