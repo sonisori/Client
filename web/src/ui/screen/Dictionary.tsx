eb033227ecd9e61e98e9a8d3141889116296a05b
Donghyun Lee <leedonghyun022@gmail.com>
2024-12-05 17:43:40 +0900

    fest: ui 개선

--- web/src/ui/screen/Dictionary.tsx
+++ web/src/ui/screen/Dictionary.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "@solidjs/router";
 
+import { ScrollArea } from "../component/base/ScrollAreaV2";
 import { SignDetector } from "../component/domain/SignDetector";
 export const Dictionary = () => {
   const navigate = useNavigate();
@@ -11,21 +12,25 @@ export const Dictionary = () => {
   return (
     <div class="fixed inset-y-0 left-72 right-0">
       <SignDetector onDone={() => navigate(-1)} open signPhraseType="평서문" />
-      <div
-        class="absolute inset-0 flex flex-col"
-        style={{ top: "calc(50vh + 70px)" }}
+      <ScrollArea
+        class="absolute inset-0"
+        direction="vertical"
+        style={{
+          // sign-detector의 바닥에 붙도록
+          top: "calc(50vh + 70px)",
+        }}
       >
         <div class="grid flex-1 grid-cols-2 gap-7 p-7">
           <div class="flex flex-col">
             <p class="mb-5 text-xl font-medium text-gray-900">{word.word}</p>
             {/* video placeholder */}
+            <video autoplay class="aspect-video" controls loop src="" />
             <div class="flex-1 bg-gray-100" />
           </div>
           <div>
             <p class="mb-3 font-medium text-gray-900">수형 사진</p>
             <div class="flex gap-3">
-              <div class="h-32 w-32 bg-gray-100" />
-              <div class="h-32 w-32 bg-gray-100" />
+              <img alt="" class="size-32 object-contain" src="" />
             </div>
             <p class="mb-3 mt-5 font-medium text-gray-900">수형 설명</p>
             <p class="text-gray-800">
@@ -34,7 +39,7 @@ export const Dictionary = () => {
             </p>
           </div>
         </div>
-      </div>
+      </ScrollArea>
     </div>
   );
 };
