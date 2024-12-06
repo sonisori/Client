f34256a59621f81ada31ca7a03ce9a8e5a0d5bb9
Donghyun Lee <leedonghyun022@gmail.com>
2024-12-06 16:41:49 +0900

    feat: api 연동

--- web/src/ui/screen/DictionaryList.tsx
+++ web/src/ui/screen/DictionaryList.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "@solidjs/router";
-import { For } from "solid-js";
+import { createResource, For } from "solid-js";
 
+import { useAuth } from "../../service/hook/useAuth";
+import { client } from "../../service/util/api";
 import {
   Table,
   TableBody,
@@ -13,16 +15,11 @@ import {
 
 export const DictionaryList = () => {
   const navigate = useNavigate();
-  const words = [
-    {
-      word: "안녕",
-      id: 1,
-    },
-    {
-      word: "배",
-      id: 2,
-    },
-  ];
+  useAuth({ goToWeb: true });
+  const [data] = createResource(() =>
+    client.get("api/words").json<{ id: number; word: string }[]>(),
+  );
+
   return (
     <div class="fixed inset-y-0 left-72 right-0 overflow-y-scroll">
       <div class="px-20 py-12">
@@ -31,6 +28,19 @@ export const DictionaryList = () => {
           <p class="text text-muted-foreground">AI와 함께 배우는 수어</p>
         </div>
         <div class="pl-5 pt-10">
+          <div class="mb-10 rounded-lg bg-gray-50 p-5">
+            <p class="mb-3 font-medium text-gray-900">
+              수어•지문자•지숫자의 설명에 사용한 손가락의 번호
+            </p>
+            <div class="flex gap-5">
+              <img alt="손가락 번호" src="/asset/hand-num.png" />
+              <p class="break-keep text-gray-800">
+                수어 / 지문자 / 지숫자의 설명에 손가락을 사용할 경우 손가락의
+                이름을 사용하지 않고 손가락의 번호를 사용하였다. 사용한 손가락의
+                번호는 다음과 같다.
+              </p>
+            </div>
+          </div>
           <Table class="w-full">
             <TableCaption>사용 가능한 수어 모음입니다.</TableCaption>
             <TableHeader>
@@ -40,7 +50,7 @@ export const DictionaryList = () => {
               </TableRow>
             </TableHeader>
             <TableBody>
-              <For each={words}>
+              <For each={data()}>
                 {(word) => (
                   <TableRow
                     class="cursor-pointer"
@@ -55,19 +65,6 @@ export const DictionaryList = () => {
               </For>
             </TableBody>
           </Table>
-          <div class="mt-14 rounded-lg bg-gray-50 p-5">
-            <p class="mb-3 font-medium text-gray-900">
-              수어•지문자•지숫자의 설명에 사용한 손가락의 번호
-            </p>
-            <div class="flex gap-5">
-              <img alt="손가락 번호" src="/asset/hand-num.png" />
-              <p class="break-keep text-gray-800">
-                수어 / 지문자 / 지숫자의 설명에 손가락을 사용할 경우 손가락의
-                이름을 사용하지 않고 손가락의 번호를 사용하였다. 사용한 손가락의
-                번호는 다음과 같다.
-              </p>
-            </div>
-          </div>
         </div>
       </div>
     </div>
